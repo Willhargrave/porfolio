002c16d2f6e4c18042e0de67f335ff8e1c776d55
will <willhargrave@hotmail.co.uk>
2023-02-12 16:47:04 +0900

    Changed background, added frames

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,11 @@
 import "./style/Project.css"
 const ProjectCard = (props) => {
     return (
-        <div className="Project-card">
-            <h2>{props.Project.name}</h2>
-            <img src={props.Project.img} alt='' className='ProjectImg' />
+        <div className="Project-all">
+            <div className="Project-card">
+                <img src={props.Project.img} alt='' className='ProjectImg' />
+                </div>
+                <h2>{props.Project.name}</h2>
                 <h3 className="info">{props.Project.info}</h3>
         </div>
 
