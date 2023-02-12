198e011bfc85ec15bcdf58d9c61dafaba43ce840
will <willhargrave@hotmail.co.uk>
2023-02-12 17:58:10 +0900

    profile boarder

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,9 @@ function About() {
             <div className="about_content">
             <div className="Head">
             <h1>Will Hargrave <hr></hr> Full Stack Engineer</h1>
+            <div className="about-image">
             <img src={require('./images/profile.jpg')}alt="profile" className='image'></img>
+            </div>
             </div>
             <p>Born in the UK. Currently living in Tokyo, please take a look at some the projects I've been working on!</p>
             </div>
