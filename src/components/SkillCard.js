0f5a6661ee96c86f8d886c487e950cedf0a73cda
will <willhargrave@hotmail.co.uk>
2023-02-12 18:12:36 +0900

    background

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,7 +1,9 @@
 const SkillCard = (props) => {
     return (
         <div className="Card">
+        <div className="Skill-img">
         <img src={props.Skill.img} alt='' className='CardImg'/>
+        </div>
         <h3>{props.Skill.name}</h3>
         </div>
         
