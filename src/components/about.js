8fe1c9f0238417663883595d5bbd26e1ff43b553
will <willhargrave@hotmail.co.uk>
2023-02-13 15:09:12 +0900

    re-size

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,22 @@
 import "./style/about.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 function About() {
-    return(
+    return (
         <div className="about" id="about">
             <div className="about_content">
-            <div className="Head">
-            <h1>Will Hargrave <hr></hr> Full Stack Engineer</h1>
-            <div className="about-image">
-            <img src={require('./images/profile.jpg')}alt="profile" className='image'></img>
-            </div>
-            </div>
-            <p>Born in the UK. Currently living in Tokyo, please take a look at some the projects I've been working on!</p>
+                <div className="Head">
+                    <h1>Will Hargrave <hr></hr> Full Stack Engineer</h1>
+                    <div className="about-image">
+                        <img src={require('./images/profile.jpg')} alt="profile" className='image'></img>
+                    </div>
+                </div>
+                <p>Born in the UK. Currently living in Tokyo, please take a look at some the projects I've been working on!</p>
+                <div className="links">
+                    <a className="button" href="https://www.linkedin.com/in/will-r-hargrave/"><FontAwesomeIcon icon="fa-brands fa-linkedin" />Linkedin</a>
+                    <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                    <a className="button" href="https://github.com/Willhargrave">Github</a>
+                    <a className="button" href="">Resume</a>
+                </div>
             </div>
         </div>
     )
